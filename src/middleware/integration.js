99aee129a6baefbb77bbd58f62fefabd472f902e
gautambnsl <bansalgautam005@gmail.com>
2024-09-10 20:27:12 +0530

    feat : adding new feat

--- src/middleware/integration.js
+++ src/middleware/integration.js
@@ -19,7 +19,7 @@ export const claimTokens = async (amount) => {
         console.log("test3")
         const signer = provider.getSigner();
         console.log("test4")
-        const contract = new ethers.Contract("0x196F2ED79A910c9091BFC41795CfF6D0Fd671c15", p2eabi, signer);
+        const contract = new ethers.Contract("0xde104384697D1318652042c42bbf34e2206B2fd8", p2eabi, signer);
         console.log("test5")
 
         // Call the claim function with the specified amount
@@ -36,3 +36,40 @@ export const claimTokens = async (amount) => {
     //   setErrorMessage('MetaMask not found');
     }
   };
+
+
+  // Function to fetch all-time top 5 ranking
+  const fetchAllTimeRanking = async () => {
+    try {
+      
+      const provider = new ethers.providers.JsonRpcProvider("https://testnet-rpc.meld.com");
+      const contract = new ethers.Contract("0xde104384697D1318652042c42bbf34e2206B2fd8", p2eabi, provider);
+      const [addresses, scores] = await contract.getAllTimeTop5();
+      const ranking = addresses.map((addr, i) => ({
+        address: addr,
+        score: ethers.utils.formatUnits(scores[i], 18)
+      }));
+     return ranking;
+    } catch (error) {
+      console.error("Error fetching all-time ranking:", error);
+    }
+  };
+
+  // Function to fetch last 7 days top 5 ranking
+  const fetchLast7DaysRanking = async () => {
+    try {
+      const provider = new ethers.providers.JsonRpcProvider("https://testnet-rpc.meld.com");
+      const contract = new ethers.Contract("0xde104384697D1318652042c42bbf34e2206B2fd8", p2eabi, provider);
+
+      const [addresses, scores] = await contract.getLast7DaysTop5();
+      const ranking = addresses.map((addr, i) => ({
+        address: addr,
+        score: ethers.utils.formatUnits(scores[i], 18)
+      }));
+      return ranking;
+    } catch (error) {
+      console.error("Error fetching last 7 days ranking:", error);
+    }
+  };
+
+  
